b0484f2a788edabad2eef450dd219304f1078bb9
SalmaMoujahid <63347708+SalmaMoujahid@users.noreply.github.com>
2020-05-07 23:30:43 +0000

    Update txnscript.java

--- txnscript.java
+++ txnscript.java
@@ -14,7 +14,7 @@ import java.sql.*;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Paths; 
 import java.util.stream.Stream;
 
 public class txnscript
